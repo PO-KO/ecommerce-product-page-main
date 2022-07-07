b8b770721aaad05f861bf5983bce2cb3c5ea428a
PO-KO <ayoub18daoui@gmail.com>
2022-07-07 13:47:10 +0100

    mmmm

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ let cartNotif = document.querySelector(".notification");
 let addBtn = document.querySelector(".btn-add");
 let iconCart = document.querySelector(".icon");
 let cartBox = document.querySelector(".cart-box");
+let quantityInCart = document.querySelector(".quantity-in-cart");
+let deleteBtn = document.querySelector(".delete-btn");
 
 thumbnails.forEach((thumbnail) => {
   thumbnail.addEventListener("click", () => {
@@ -28,10 +30,11 @@ menuBtn.addEventListener("click", showNav);
 closeBtn.addEventListener("click", hideNav);
 quantityPlus.addEventListener("click", plusQuantity);
 quantityMinus.addEventListener("click", minusQuantity);
-addBtn.addEventListener("click", addToCart);
+addBtn.addEventListener("click", addToCartNotif);
 iconCart.addEventListener("click", toggleCart);
+deleteBtn.addEventListener("click", deleteFromCart);
 
-addToCart();
+addToCartNotif();
 
 function preview(num) {
   let img = document.createElement("img");
@@ -63,16 +66,45 @@ function addToQuantityBlock(quantity) {
   quantityBlock.innerHTML = quantity;
 }
 
-function addToCart() {
+function addToCartNotif() {
   if (quantity === 0) {
     cartNotif.classList.add("empty");
+    addToCart();
     return;
   }
 
   cartNotif.classList.remove("empty");
   cartNotif.innerHTML = quantity;
+  addToCart();
 }
 
 function toggleCart() {
   cartBox.classList.toggle("show");
+}
+
+function addToCart() {
+  if (quantity === 0) {
+    cartBox.classList.add("empty");
+    cartBox.querySelector(".row").append("Your cart is empty");
+  } else {
+    cartBox.querySelector(".row").lastChild.remove();
+    cartBox.classList.remove("empty");
+    let price = document.querySelector(".current-price p").innerHTML;
+    let resultSpan = document.createElement("span");
+    let result = +price.slice(1) * quantity;
+    resultSpan.classList.add("result");
+
+    quantityInCart.innerHTML = "";
+
+    quantityInCart.append(`${price} x ${quantity}`);
+    resultSpan.innerHTML = `$${result}.00`;
+    quantityInCart.append(resultSpan);
+  }
+}
+
+function deleteFromCart() {
+  cartBox.classList.add("empty");
+  quantity = 0;
+  addToCartNotif();
+  addToQuantityBlock(quantity);
 }
